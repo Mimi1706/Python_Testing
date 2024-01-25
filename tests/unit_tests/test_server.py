8ca989efbf1561e384200c6029bf3136e5676f43
Mimi <hana.nguyen@outlook.fr>
2024-01-25 23:01:44 +0100

    Adding valid places purchase test

--- tests/unit_tests/test_server.py
+++ tests/unit_tests/test_server.py
@@ -17,3 +17,15 @@ class TestShowSummary:
 
         assert response.status_code == 200
         assert b"Email not found." in response.data
+        
+class TestPurchasePlaces:
+    def test_valid_purchase(self, client, clubs, competitions):
+        competition = competitions[0]["name"]
+        club = clubs[0]["name"]
+        places_to_book = 10
+
+        response = client.post("/purchasePlaces", data={"club": club, "competition": competition, "places": places_to_book})
+
+        assert response.status_code == 200
+        assert b"Great, booking complete!" in response.data
+        assert b"Points available: 3" in response.data
