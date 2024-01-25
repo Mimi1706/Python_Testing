047d2eca3bfffff5e0b0b5b15cb14c2f0fec4cd3
Mimi <hana.nguyen@outlook.fr>
2024-01-25 23:42:25 +0100

    Adding invalid purchases tests

--- tests/unit_tests/test_server.py
+++ tests/unit_tests/test_server.py
@@ -1,11 +1,38 @@
 class TestPurchasePlaces:
     def test_valid_purchase(self, client, clubs, competitions):
-        competition = competitions[0]["name"]
-        club = clubs[0]["name"]
+        competition_name = competitions[1]["name"] # competition 2 has 13 places
+        club_name = clubs[0]["name"] # club 1 only has 13 balance points
         places_to_book = 10
 
-        response = client.post("/purchasePlaces", data={"club": club, "competition": competition, "places": places_to_book})
+        response = client.post("/purchasePlaces", data={"club": club_name, "competition": competition_name, "places": places_to_book})
 
-        assert response.status_code == 200
         assert b"Great, booking complete!" in response.data
         assert b"Points available: 3" in response.data
+
+    def test_invalid_purchase_not_enough_points(self, client, clubs, competitions):
+        competition_name = competitions[0]["name"]
+        club_name = clubs[1]["name"] # club 2 only has 4 balance points
+        places_to_book = 5
+
+        response = client.post("/purchasePlaces", data={"club": club_name, "competition": competition_name, "places": places_to_book})
+
+        assert b"Your point balance is not enough." in response.data
+        assert b"Points available: 4" in response.data
+
+    def test_invalid_purchase_not_enough_places(self, client, clubs, competitions):
+        competition_name = competitions[1]["name"] # competition 2 has 3 places left after test_valid_purchase
+        club_name = clubs[2]["name"] # club 3 has 12 balance points
+        places_to_book = 10
+
+        response = client.post("/purchasePlaces", data={"club": club_name, "competition": competition_name, "places": places_to_book})
+
+        assert b"Not enough places available for the quantity you requested." in response.data
+
+    def test_invalid_purchase_invalid_value(self, client, clubs, competitions):
+        competition_name = competitions[1]["name"] # competition 2 has 3 places left after test_valid_purchase
+        club_name = clubs[2]["name"] # club 3 has 12 balance points
+        places_to_book = -2
+
+        response = client.post("/purchasePlaces", data={"club": club_name, "competition": competition_name, "places": places_to_book})
+
+        assert b"Incorrect value." in response.data
